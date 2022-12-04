9611c5d13f539d4af74d26237e5a139609071db5
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-04 14:08:35 +0300

    Сделала задачу 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number = ReadInt("Введите число: ");
+
+int result = Sum(number);
+
+Console.WriteLine($"Сумма цифр числа {number} равняется: {result}");
+
+
+int Sum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
