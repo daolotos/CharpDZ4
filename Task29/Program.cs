2070c765fb87871798b13d459223c06d9a55d19e
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-04 14:16:00 +0300

    Сделала задачу 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 8 чисел");
+
+int count = 8;
+int[] arr = new int[count];
+
+InputNumbers(count, arr);
+
+WriteNumbers(count, arr);
+
+int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+void InputNumbers(int count, int[] arr)
+{
+    for (int i = 0; i < count; i++)
+        arr[i] = ReadInt("Введите число: ");
+}
+
+void WriteNumbers(int count, int[] arr)
+{
+    for (int i = 0; i < count; i++)
+        Console.Write(arr[i] + " ");
+}
